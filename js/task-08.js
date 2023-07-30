dcde5a382109fb82db13f70ec6ea2850efe73303
krystianklimczak <krystianklimczak1996r.com>
2023-07-30 13:03:58 +0200

    eighth ex done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
+// console.log(form);
+form.addEventListener('submit', handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return console.log(alert('Please fill all the fields!'));
+  } else {
+    const inputData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(inputData);
+    form.reset();
+  }
+}
