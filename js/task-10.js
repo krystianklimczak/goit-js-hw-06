4837180955d9e2c90fd5fd5d7755dd9bc6e76b78
krystianklimczak <krystianklimczak1996r.com>
2023-07-30 14:03:58 +0200

    tenth ex done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input[type="number"]');
+const boxOfBoxes = document.querySelector('#boxes');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+// console.log(destroyBtn);
+// console.log(input);
+let boxSize = 30;
+
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxOfBoxes.append(box);
+    boxSize = boxSize + 10;
+  }
+}
+
+createBtn.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener('click', () => {
+  while (boxOfBoxes.firstChild) {
+    boxOfBoxes.removeChild(boxOfBoxes.lastChild);
+  }
+  boxSize = 30;
+});
