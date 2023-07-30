7ce24b7ac4b7f79452dc710a9f0e5e12aa8b4be3
krystianklimczak <krystianklimczak1996r.com>
2023-07-30 13:13:35 +0200

    ninth ex done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const siteBody = document.querySelector('body');
+const paragraphOfColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+  siteBody.style.backgroundColor = color;
+  paragraphOfColor.textContent = color;
+}
